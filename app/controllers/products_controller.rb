11c2b1da735d110dadcd30feefcf8f353712ff23
Lianne Gabriel <liannegabriel519@yahoo.com>
2024-10-22 19:21:04 -0500

    Created the show and modified other files

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  def show
+    @products = Product.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :products, only: %i[index]
+  resources :products, only: %i[index show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
